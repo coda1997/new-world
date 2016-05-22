179ddd45b51ef8ccc6497fae23f2c3dd1bf2544e
604405145@qq.com <604405145@qq.com>
2016-05-22 10:01:00 +0800

    Accept ！

--- Exercise set/POJ 1753.cpp
+++ Exercise set/POJ 1753.cpp
@@ -1,75 +1,66 @@
 #include<iostream>
-using std::cout;
-int all_whiteOr_all_black(int *arr,int size) {
-    for (int i = 0; i < size - 1; i++) {
-        if (arr[i] != arr[i + 1])
-            return 0;
-    }
-    return 1;
-}
-void change(int *arr,int changeNum) {//to change the color, it is right
-    arr[changeNum] = !arr[changeNum];
-    int x = changeNum / 4;
-    int y = changeNum % 4;
-    if (x > 0) arr[changeNum - 4] = !arr[changeNum - 4];
-    if (x < 4)arr[changeNum + 4] = !arr[changeNum + 4];
-    if (y > 0)arr[changeNum - 1] = !arr[changeNum - 1];
-    if (y < 3)arr[changeNum + 1] = !arr[changeNum + 1];
-}
+using namespace std;
+bool chess[6][6] = { false };
+bool flag;
+int step;
+int r[] = { -1,1,0,0,0 };
+int c[] = { 0,0,-1,1,0 };
 
-int arrEnum(int *arr2, int *result,const int length, int start,int *arr) {
-    for (int i = start; i < 16 - length + 1; i++) {
-        result[i] = i;
-        int* copy_arr = new int[16];
-        for (int n = 0; n < 16; n++)
-            copy_arr[n] = arr[n];
-        if (length - 1 == 0) {//make  a cpoy to arr the new new_arr
-            int *new_arr = new int[length];
-            for (int i = 0; i < length; i++) {
-                new_arr[i] = arr2[result[i]];
-            }
-            for (int i = 0; i < length; i++) {
-                change(copy_arr, new_arr[i]);
-            }
-            if (all_whiteOr_all_black(copy_arr, 16)==1) {
-                free(new_arr);
-                return length;
-            }
-            else
-            {
-                free(new_arr);
-            }
-        }
-        else {
-            arrEnum(arr2, result, length - 1, i+1,arr);
+bool judge_all(void) {
+    int i, j;
+    for (i = 1; i<5; i++) {
+        for (j = 1; j<5; j++)
+            if (chess[i][j] != chess[1][1])
+                return false;
 
-        }
-        free(copy_arr);
     }
-    return 0;
+    return true;
 }
 
-int main() {
-    int arr[] = { 0,1,1,0,0,0,1,0,0,1,1,0,0,1,1,1};
-    int arr2[] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-    int reu[16];
-    int size = 16;
-    int step = 0;
-    if (all_whiteOr_all_black(arr, size)) {//length ==0
-        cout << "0 step";
-        system("pause");
+void flip(int row, int col) {
+    for (int i = 0; i<5; i++)
+        chess[row + r[i]][col + c[i]] = !chess[row + r[i]][col + c[i]];
+}
 
-        return 0;
+void dfs(int row, int col, int deep) {
+    if (deep == step) {
+        flag = judge_all();
+        return;
     }
-    for (int i = 1; i < 17; i++) {//i==length
-
-        step = arrEnum(arr2, reu, i, 0, arr);
-    }
-    if (step != 0)
-        cout << step << " step" << std::endl;
+    if (flag==true || row == 5)
+        return;
+    flip(row, col);
+    if (col<4)
+        dfs(row, col + 1, deep + 1);
     else
-        cout << "impossible !";
+        dfs(row + 1, col-3, deep + 1);//the col-3 is important !
+    flip(row,col);
+    if (col<4)
+        dfs(row, col + 1, deep);
+    else
+        dfs(row + 1, col-3, deep);
+    return;
+}
+int main() {
+    char temp;
+    for (int i = 1; i < 5; i++) {
+        for (int j = 1; j < 5; j++) {
+            cin >> temp;
+            if (temp == 'b')
+                chess[i][j] = true;
+        }
+    }
+    for (step = 0; step <= 16; step++) {
+        dfs(1, 1, 0);
+        if (flag)
+            break;
+    }
+    if (flag)
+        cout << step << endl;
+    else
+        cout << "Impossible" << endl;
     system("pause");
     return 0;
+
 }
-//forget to make a copy for arr itself because of the change 
+
